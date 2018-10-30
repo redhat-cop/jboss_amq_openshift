82a763818a184bff88f7f675021b9643be9d3c98
Christian Polizzi <christian.polizzi@redhat.com>
2018-10-30 16:09:36 -0700

    New embedded module to retrieve all certificates from a PKCS#12 file; updated role to exploit new embedded module

---
+++ site/roles/openshift-amq/library/openssl_pksc12_certificate_chain.py
@@ -0,0 +1,78 @@
+from ansible.module_utils.basic import *
+from OpenSSL import crypto
+
+#
+# TODO
+#   - `cert_number` is a quick workaround to provide a unique name for the certificate
+#   - `chain[i].name` is  quick workaroud to to provide the name for the certificate
+#        o This should be replaced by the CN of the certificate, or:
+#            * provide a dictionary of the certificate subject (and issuer) for each name compoenent
+#
+
+def main():
+    args = {
+        "src": { "required": True, "type": "str" },
+        "passphrase": { "required": False, "type": "str" },
+    }
+
+    module = AnsibleModule(argument_spec=args)
+
+    #### MODULE LOGIC
+
+    # Load PKCS#12 file
+    p12 = crypto.load_pkcs12(open(module.params["src"], 'rb').read(), module.params["passphrase"])
+
+    chain = list()
+    cert_number = 1   # TODO See module note above
+
+    # Obtain the signed certificate
+    cert = p12.get_certificate()
+    c = {
+        "certificate": crypto.dump_certificate(crypto.FILETYPE_PEM, cert),
+        "fingerprint": {
+                "algorithm": "SHA1",
+                "digest": cert.digest("SHA1"),
+            },
+        "has_expired": cert.has_expired(),
+        "issuer": ", ".join("%s=%s" % tup for tup in cert.get_issuer().get_components()),
+        "serial_number": cert.get_serial_number(),
+        "signature_algorithm": cert.get_signature_algorithm(),
+        "subject": ", ".join("%s=%s" % tup for tup in cert.get_subject().get_components()),
+        "validity": {
+                "not_after": cert.get_notAfter(),
+                "not_before": cert.get_notBefore(),
+            },
+        "version": cert.get_version() + 1,
+    }
+    c["name"] = "cert-{}".format(cert_number) # TODO See module note above
+    cert_number += 1
+    chain.append(c)
+
+    # Obtain CA certificate chain
+    for cert in p12.get_ca_certificates():
+        c = {
+            "certificate": crypto.dump_certificate(crypto.FILETYPE_PEM, cert),
+            "fingerprint": {
+                    "algorithm": "SHA1",
+                    "digest": cert.digest("SHA1"),
+                },
+            "has_expired": cert.has_expired(),
+            "issuer": ", ".join("%s=%s" % tup for tup in cert.get_issuer().get_components()),
+            "serial_number": cert.get_serial_number(),
+            "signature_algorithm": cert.get_signature_algorithm(),
+            "subject": ", ".join("%s=%s" % tup for tup in cert.get_subject().get_components()),
+            "validity": {
+                    "not_after": cert.get_notAfter(),
+                    "not_before": cert.get_notBefore(),
+                },
+            "version": cert.get_version() + 1,
+        }
+        c["name"] = "cert-{}".format(cert_number) # TODO See module note above
+        cert_number += 1
+        chain.append(c)
+
+    chain.reverse()
+    module.exit_json(changed=True, certificates=chain)
+
+if __name__ == '__main__':
+    main()
