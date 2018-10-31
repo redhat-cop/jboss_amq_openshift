d867693dfc5e1e9be2cc4a954cb82adfedbe6548
Christian Polizzi <christian.polizzi@redhat.com>
2018-10-30 18:29:42 -0700

    Streamlined decode of X.509 certificates

--- site/roles/openshift-amq/library/openssl_pksc12_certificate_chain.py
+++ site/roles/openshift-amq/library/openssl_pksc12_certificate_chain.py
@@ -12,24 +12,7 @@ from OpenSSL import crypto
 def x509name_str(x509name):
     return ", ".join("%s=%s" % tup for tup in x509name.get_components())
 
-def main():
-    args = {
-        "src": { "required": True, "type": "str" },
-        "passphrase": { "required": False, "type": "str" },
-    }
-
-    module = AnsibleModule(argument_spec=args)
-
-    #### MODULE LOGIC
-
-    # Load PKCS#12 file
-    p12 = crypto.load_pkcs12(open(module.params["src"], 'rb').read(), module.params["passphrase"])
-
-    chain = list()
-    cert_number = 1   # TODO See module note above
-
-    # Obtain the signed certificate
-    cert = p12.get_certificate()
+def x509cert_decode(cert):
     c = {
         "certificate": crypto.dump_certificate(crypto.FILETYPE_PEM, cert),
         "fingerprint": {
@@ -47,6 +30,27 @@ def main():
             },
         "version": cert.get_version() + 1,
     }
+
+    return c
+
+def main():
+    args = {
+        "src": { "required": True, "type": "str" },
+        "passphrase": { "required": False, "type": "str" },
+    }
+
+    module = AnsibleModule(argument_spec=args)
+
+    #### MODULE LOGIC
+
+    # Load PKCS#12 file
+    p12 = crypto.load_pkcs12(open(module.params["src"], 'rb').read(), module.params["passphrase"])
+
+    chain = list()
+    cert_number = 1   # TODO See module note above
+
+    # Obtain the signed certificate
+    c = x509cert_decode(p12.get_certificate())
     c["name"] = "cert-{}".format(cert_number) # TODO See module note above
     cert_number += 1
     chain.append(c)
@@ -54,23 +58,7 @@ def main():
     # Obtain CA certificate chain
     if p12.get_ca_certificates() is not None:
         for cert in p12.get_ca_certificates():
-            c = {
-                "certificate": crypto.dump_certificate(crypto.FILETYPE_PEM, cert),
-                "fingerprint": {
-                        "algorithm": "SHA1",
-                        "digest": cert.digest("SHA1"),
-                    },
-                "has_expired": cert.has_expired(),
-                "issuer": x509name_str(cert.get_issuer()),
-                "serial_number": cert.get_serial_number(),
-                "signature_algorithm": cert.get_signature_algorithm(),
-                "subject": x509name_str(cert.get_subject()),
-                "validity": {
-                        "not_after": cert.get_notAfter(),
-                        "not_before": cert.get_notBefore(),
-                    },
-                "version": cert.get_version() + 1,
-            }
+            c = x509cert_decode(p12.get_certificate())
             c["name"] = "cert-{}".format(cert_number) # TODO See module note above
             cert_number += 1
             chain.append(c)
